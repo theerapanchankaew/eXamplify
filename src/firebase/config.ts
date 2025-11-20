3a9582dc8dd9e26d6b6982198e2c3b2c2c73f1a3
Theerapan C <theerapanMASCI@gmail.com>
2025-11-20 04:10:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5931178696-f178e",
+  "appId": "1:840014468456:web:37c6c7b0dbc8444d9af2dd",
+  "apiKey": "AIzaSyB2En6Ng4S3boY0ZX-ogk7Hu5cAlAZjM4M",
+  "authDomain": "studio-5931178696-f178e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "840014468456"
+};
