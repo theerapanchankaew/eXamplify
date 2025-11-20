48ac641732986c87a88b0f3cab991720d10f65d4
Theerapan C <theerapanMASCI@gmail.com>
2025-11-20 08:29:12 +0000

    ต่อ

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -19,6 +19,4 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
 )
 Input.displayName = "Input"
 
-// Exporting CodeBlock here to make it available for use
-export { CodeBlock } from './code-block';
 export { Input }
